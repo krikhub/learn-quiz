08d5a1dc935f3091d83e138520b2bb1d74cf53da
Raiden <153776281+RaidenvBlack@users.noreply.github.com>
2025-04-26 13:35:12 +0200

    Update Question Structure

--- src/l/halbritter/Model/Question.java
+++ src/l/halbritter/Model/Question.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 
 public class Question {
 
+    private int questionId;
     private String questionText;
     private ArrayList<String> answers;
     private int correctIndex;
     private String topic;
     private int difficulty;
 
-    Question(String questionText, ArrayList<String> answers, int correctIndex, String topic, int difficulty) {
+    Question(int questionId, String questionText, ArrayList<String> answers, int correctIndex, String topic, int difficulty) {
+        this.questionId = questionId;
         this.questionText = questionText;
         this.answers = answers;
         this.correctIndex = correctIndex;

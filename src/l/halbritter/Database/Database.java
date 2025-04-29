158fe71fa789d13a05804041eabf4dc7b3d363ab
Raiden <153776281+RaidenvBlack@users.noreply.github.com>
2025-04-29 21:14:54 +0200

    Dynamische Topics
    Für die einfache Erweiterbarkeit laden wir die Topics auch aus einer json so kann man sie einfach erweitern/verkleinern

--- src/l/halbritter/Database/Database.java
+++ src/l/halbritter/Database/Database.java
@@ -16,7 +16,19 @@ public class Database {
 
     private static final String QUESTIONS_FILE = "questions.json";
     private static final String USERS_FILE = "users.json";
+    private static final String TOPICS_FILE = "topics.json";
     private final Gson gson = new Gson();
+
+    // Load Topics from JSON
+    public List<String> loadTopics() {
+        try (FileReader reader = new FileReader(TOPICS_FILE)) {
+            Type listType = new TypeToken<List<String>>() {}.getType();
+            return gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
 
     // Load Questions from JSON
     public List<Question> loadQuestions() {

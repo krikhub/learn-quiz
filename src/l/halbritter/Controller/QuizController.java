c760bcd3980a00785dafed2f4673a0596573d909
Raiden <153776281+RaidenvBlack@users.noreply.github.com>
2025-05-21 19:32:47 +0200

    richtiges schließen der datenbank nach beendigung über das x(bei mac das rote etwas)

--- src/l/halbritter/Controller/QuizController.java
+++ src/l/halbritter/Controller/QuizController.java
@@ -9,6 +9,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.*;
 import java.util.List;
 import java.util.Map;
@@ -32,6 +34,24 @@ public class QuizController {
     public void startApplication() {
         db.connect();
         allQuestions = db.loadQuestions();
+
+        JFrame frame = view.getFrame();
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int ans = JOptionPane.showConfirmDialog(
+                        frame,
+                        "Möchtest du wirklich beenden?",
+                        "Beenden",
+                        JOptionPane.YES_NO_OPTION
+                );
+                if (ans == JOptionPane.YES_OPTION) {
+                    exitApplication();
+                }
+            }
+        });
 
         view.showMainMenu(
                 e -> showQuestionsForEditing(),
@@ -440,5 +460,10 @@ public class QuizController {
 
     private void handleCancelToMain(ActionEvent e) {
         startApplication();
+    }
+
+    private void exitApplication() {
+        db.disconnect();
+        System.exit(0);
     }
 }
